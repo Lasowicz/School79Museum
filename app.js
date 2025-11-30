5baeadb34b55014404d7556f77f7a6b5ab3af5e3
Wsewolod Lasowicz <vsevolodlyasovich@yandex.ru>
2025-11-30 18:04:22 +0500

    30.11.2025 Добавил бэкгрануд для main content, освежил меню, добавил кнопки и слушатель на календарь исторических событий

--- app.js
+++ app.js
@@ -8,3 +8,9 @@ const collage = document.getElementById("collage");
 collage.addEventListener("click", function () {
     window.open('https://ru.ruwiki.ru/wiki/Великая_Отечественная_война');
 })
+
+                                //Слушатели на клики по логотипу и коллажу
+const button_memorable_and_history_date = document.getElementById('memorable_and_history_date');
+button_memorable_and_history_date.addEventListener("click", function () {
+    console.log("memorable_and_history_date");
+})
