9bdb876a876e2bc28c8da6072af4ab41f1c9af34
Wsewolod Lasowicz <vsevolodlyasovich@yandex.ru>
2026-02-22 13:38:16 +0500

    1. Made artifacts open on click in a separate tab using target_blank

---
+++ arftifactsScale.js
@@ -0,0 +1,13 @@
+const artifactsImg = document.querySelectorAll('.artifacts_image');
+
+artifactsImg.forEach(el => {
+    el.addEventListener('mouseover', (event) => {
+
+    })
+})
+artifactsImg.forEach(el => {
+    el.addEventListener('mouseout', (event) => {
+
+    })
+})
+
